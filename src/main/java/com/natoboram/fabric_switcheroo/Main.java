344cb38740c98c9ae4a6a57d2883d534a2e5dbcf
Nato Boram <nato.boram@gmail.com>
2019-08-10 12:31:18 -0400

    Reorder stuff

--- src/main/java/com/natoboram/fabric_switcheroo/Main.java
+++ src/main/java/com/natoboram/fabric_switcheroo/Main.java
@@ -33,6 +33,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class Main implements ModInitializer {
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -73,81 +74,57 @@ public class Main implements ModInitializer {
 	}
 
 	/**
-	 * Checks if the switcheroo action should be cancelled.
-	 * 
-	 * @param player Player that's about to execute a switcheroo.
+	 * Execute a switcheroo action when attacking a block.
 	 */
-	private boolean cancelSwitcheroo(PlayerEntity player) {
-		return player.isSpectator() || player.isSneaking();
-	}
+	private ActionResult onAttackBlock(PlayerEntity player, World world, Hand hand, BlockPos pos, Direction direction) {
 
-	/**
-	 * Removes enchanted items that have only one durability left from the
-	 * switcheroo.
-	 */
-	private void preserveEnchantedItems(ArrayList<ItemStack> items) {
-		items.removeIf((item) -> {
-			return !item.getEnchantments().isEmpty() && item.getMaxDamage() - item.getDamage() <= 1;
+		BlockState block = world.getBlockState(pos);
+		ArrayList<ItemStack> tools = new ArrayList<ItemStack>();
+
+		// Get all effective tools from the inventory
+		player.inventory.main.forEach((item) -> {
+			if (item.isEffectiveOn(block)) {
+				tools.add(item);
+			}
 		});
-	}
 
-	/**
-	 * Only keep the most damaged items.
-	 */
-	private void mostDamagedItems(ArrayList<ItemStack> items) {
-		float max = items.stream().max(Comparator.comparing(item -> item.getDamage())).get().getDamage();
-		items.removeIf((item) -> max > item.getDamage());
-	}
-
-	/**
-	 * Perform the actual switcheroo.
-	 * 
-	 * @param player PlayerEntity that's about to execute the switcheroo.
-	 * @param item   Item that should be but in its hand.
-	 */
-	private void switcheroo(PlayerEntity player, ItemStack item) {
-
-		// Only works in single player because it actually edits the world.
-		// Collections.swap(player.inventory.main, player.inventory.main.indexOf(item),
-		// player.inventory.main.indexOf(player.getStackInHand(Hand.MAIN_HAND)));
-
-		// Don't send useless packets
-		if (player.inventory.getMainHandStack().isItemEqualIgnoreDamage(item)) {
-			return;
+		// If there's no effective tools, check for the mining speed.
+		if (tools.isEmpty()) {
+			player.inventory.main.forEach((item) -> {
+				if (item.getMiningSpeed(block) > 1.0F) {
+					tools.add(item);
+				}
+			});
 		}
 
-		MinecraftClient.getInstance().interactionManager.pickFromInventory(player.inventory.getSlotWithStack(item));
-	}
+		// Filters enchanted items with 1 durability
+		removeDamagedEnchantedItems(tools);
 
-	/**
-	 * Gets the damage that would be done by an ItemStack to an EntityGroup.
-	 */
-	private float getDamage(ItemStack stack, EntityGroup entityGroup) {
-		float damage = 0;
-
-		// Stack Enchantments
-		damage += EnchantmentHelper.getAttackDamage(stack, entityGroup);
-
-		// Item Modifiers
-		Item item = stack.getItem();
-		item.getModifiers(EquipmentSlot.MAINHAND).get(EntityAttributes.ATTACK_DAMAGE.getId()).stream()
-				.mapToDouble(EntityAttributeModifier::getAmount).sum();
-
-		// Sword Attack Damage
-		if (item instanceof SwordItem) {
-			SwordItem sword = (SwordItem) item;
-			damage += sword.getAttackDamage();
+		// Safety before launching streams
+		if (tools.isEmpty()) {
+			return ActionResult.PASS;
 		}
 
-		return damage;
-	}
+		// Get best or worst tool
+		if (MinecraftClient.getInstance().options.keySprint.isPressed()) {
+			float max = tools.stream().max(Comparator.comparing(item -> item.getMiningSpeed(block))).get()
+					.getMiningSpeed(block);
+			tools.removeIf((item) -> max > item.getMiningSpeed(block));
+		} else {
+			float min = tools.stream().min(Comparator.comparing(item -> item.getMiningSpeed(block))).get()
+					.getMiningSpeed(block);
+			tools.removeIf((item) -> min < item.getMiningSpeed(block));
+		}
 
-	/**
-	 * Only keep the lowest stacks.
-	 */
-	private void keepLowestStacks(ArrayList<ItemStack> items) {
-		float min = items.stream().min(Comparator.comparing(item -> item.getCount())).get().getDamage();
-		items.removeIf((item) -> min < item.getCount());
+		// Get most damaged item
+		keepMostDamagedItems(tools);
+
+		if (tools.isEmpty()) {
+			return ActionResult.PASS;
+		}
+
+		switcheroo(player, tools.get(0));
+		return ActionResult.PASS;
 	}
 
 	/**
@@ -200,60 +177,6 @@ public class Main implements ModInitializer {
 	}
 
 	/**
-	 * Execute a switcheroo action when attacking a block.
-	 */
-	private ActionResult onAttackBlock(PlayerEntity player, World world, Hand hand, BlockPos pos, Direction direction) {
-
-		BlockState block = world.getBlockState(pos);
-		ArrayList<ItemStack> tools = new ArrayList<ItemStack>();
-
-		// Get all effective tools from the inventory
-		player.inventory.main.forEach((item) -> {
-			if (item.isEffectiveOn(block)) {
-				tools.add(item);
-			}
-		});
-
-		// If there's no effective tools, check for the mining speed.
-		if (tools.isEmpty()) {
-			player.inventory.main.forEach((item) -> {
-				if (item.getMiningSpeed(block) > 1.0F) {
-					tools.add(item);
-				}
-			});
-		}
-
-		// Filters enchanted items with 1 durability
-		preserveEnchantedItems(tools);
-
-		// Safety before launching streams
-		if (tools.isEmpty()) {
-			return ActionResult.PASS;
-		}
-
-		// Get best or worst tool
-		if (MinecraftClient.getInstance().options.keySprint.isPressed()) {
-			float max = tools.stream().max(Comparator.comparing(item -> item.getMiningSpeed(block))).get()
-					.getMiningSpeed(block);
-			tools.removeIf((item) -> max > item.getMiningSpeed(block));
-		} else {
-			float min = tools.stream().min(Comparator.comparing(item -> item.getMiningSpeed(block))).get()
-					.getMiningSpeed(block);
-			tools.removeIf((item) -> min < item.getMiningSpeed(block));
-		}
-
-		// Get most damaged item
-		mostDamagedItems(tools);
-
-		if (tools.isEmpty()) {
-			return ActionResult.PASS;
-		}
-
-		switcheroo(player, tools.get(0));
-		return ActionResult.PASS;
-	}
-
-	/**
 	 * Execute a switcheroo action when attacking an entity.
 	 */
 	private ActionResult onAttackEntity(PlayerEntity player, World world, Hand hand, Entity entity,
@@ -269,7 +192,7 @@ public class Main implements ModInitializer {
 		});
 
 		// Filters enchanted items with 1 durability
-		preserveEnchantedItems(swords);
+		removeDamagedEnchantedItems(swords);
 
 		LivingEntity livingEntity = (LivingEntity) entity;
 		EntityGroup entityGroup = livingEntity.getGroup();
@@ -291,7 +214,7 @@ public class Main implements ModInitializer {
 		}
 
 		// Get most damaged items
-		mostDamagedItems(swords);
+		keepMostDamagedItems(swords);
 
 		if (swords.isEmpty()) {
 			return ActionResult.PASS;
@@ -300,4 +223,83 @@ public class Main implements ModInitializer {
 		switcheroo(player, swords.get(0));
 		return ActionResult.PASS;
 	}
+
+	/**
+	 * Checks if the switcheroo action should be cancelled.
+	 * 
+	 * @param player Player that's about to execute a switcheroo.
+	 */
+	private boolean cancelSwitcheroo(PlayerEntity player) {
+		return player.isSpectator() || player.isSneaking();
+	}
+
+	/**
+	 * Gets the damage that would be done by an ItemStack to an EntityGroup.
+	 */
+	private float getDamage(ItemStack stack, EntityGroup entityGroup) {
+		float damage = 0;
+
+		// Stack Enchantments
+		damage += EnchantmentHelper.getAttackDamage(stack, entityGroup);
+
+		// Item Modifiers
+		Item item = stack.getItem();
+		item.getModifiers(EquipmentSlot.MAINHAND).get(EntityAttributes.ATTACK_DAMAGE.getId()).stream()
+				.mapToDouble(EntityAttributeModifier::getAmount).sum();
+
+		// Sword Attack Damage
+		if (item instanceof SwordItem) {
+			SwordItem sword = (SwordItem) item;
+			damage += sword.getAttackDamage();
+		}
+
+		return damage;
+	}
+
+	/**
+	 * Removes enchanted items that have only one durability left from the
+	 * switcheroo.
+	 */
+	private void removeDamagedEnchantedItems(ArrayList<ItemStack> items) {
+		items.removeIf((item) -> {
+			return !item.getEnchantments().isEmpty() && item.getMaxDamage() - item.getDamage() <= 1;
+		});
+	}
+
+	/**
+	 * Only keep the most damaged items.
+	 */
+	private void keepMostDamagedItems(ArrayList<ItemStack> items) {
+		float max = items.stream().max(Comparator.comparing(item -> item.getDamage())).get().getDamage();
+		items.removeIf((item) -> max > item.getDamage());
+	}
+
+	/**
+	 * Only keep the lowest stacks.
+	 */
+	private void keepLowestStacks(ArrayList<ItemStack> items) {
+		float min = items.stream().min(Comparator.comparing(item -> item.getCount())).get().getDamage();
+		items.removeIf((item) -> min < item.getCount());
+	}
+
+	/**
+	 * Perform the actual switcheroo.
+	 * 
+	 * @param player PlayerEntity that's about to execute the switcheroo.
+	 * @param item   Item that should be but in its hand.
+	 */
+	private void switcheroo(PlayerEntity player, ItemStack item) {
+
+		// Only works in single player because it actually edits the world.
+		// Collections.swap(player.inventory.main, player.inventory.main.indexOf(item),
+		// player.inventory.main.indexOf(player.getStackInHand(Hand.MAIN_HAND)));
+
+		// Don't send useless packets
+		if (player.inventory.getMainHandStack().isItemEqualIgnoreDamage(item)) {
+			return;
+		}
+
+		MinecraftClient.getInstance().interactionManager.pickFromInventory(player.inventory.getSlotWithStack(item));
+	}
+
 }

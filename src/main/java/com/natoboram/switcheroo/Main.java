2323b34b07e353b02a99ba434f345e8b2928c40b
Nato Boram <nato.boram@gmail.com>
2021-09-26 13:32:29 -0400

    Change mod id and version number to Switcheroo 1.0.3

--- src/main/java/com/natoboram/switcheroo/Main.java
+++ src/main/java/com/natoboram/switcheroo/Main.java
@@ -1,4 +1,4 @@
-package com.natoboram.fabric_switcheroo;
+package com.natoboram.switcheroo;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -44,7 +44,7 @@ public class Main implements ClientModInitializer {
 	 * considered best practice to use your mod id as the logger's name. That way,
 	 * it's clear which mod wrote info, warnings, and errors.
 	 */
-	public static final Logger LOGGER = LogManager.getLogger("fabric_switcheroo");
+	public static final Logger LOGGER = LogManager.getLogger("switcheroo");
 
 	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
 

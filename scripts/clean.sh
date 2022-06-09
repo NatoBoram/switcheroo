1c9ac2e1ea3616cae3d088e9f525403ccf197518
Nato Boram <nato.boram@gmail.com>
2022-06-08 20:40:16 -0400

    🔨 cleanEclipse in clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,4 +2,4 @@
 
 rm -rf .classpath .factorypath .gradle .project .settings .vscode/launch.json bin build run switcheroo_client.launch switcheroo_server.launch
 
-gradle clean downloadAssets genSources eclipse vscode
+gradle clean cleanEclipse downloadAssets genSources eclipse vscode

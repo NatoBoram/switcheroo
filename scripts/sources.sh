5cf602665437d710941f915607da955e1008d60d
Nato Boram <nato.boram+git@gmail.com>
2025-07-06 14:56:34 -0400

    🎨 Apply code review suggestion

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -r sources
-mkdir -p sources
+rm --force --recursive sources
+mkdir --parents sources
 
 cd sources
 jar --extract --file='../.gradle/loom-cache/minecraftMaven/net/minecraft/minecraft-merged-54576d3639/1.21.5-net.fabricmc.yarn.1_21_5.1.21.5+build.1-v2/minecraft-merged-54576d3639-1.21.5-net.fabricmc.yarn.1_21_5.1.21.5+build.1-v2-sources.jar'

4f1ba9e2006f2b8666e794ce3de31e3ef412281f
Nato Boram <nato.boram+git@gmail.com>
2026-03-05 00:42:18 -0500

    🥅 Identifier::tryParse

--- src/main/java/com/natoboram/switcheroo/util/Identifiers.java
+++ src/main/java/com/natoboram/switcheroo/util/Identifiers.java
@@ -1,6 +1,7 @@
 package com.natoboram.switcheroo.util;
 
 import java.util.Arrays;
+import java.util.Objects;
 import net.minecraft.util.Identifier;
 
 public class Identifiers {
@@ -8,8 +9,8 @@ public class Identifiers {
 	/** Check if the given identifier is present in a space-separated string list. */
 	public static boolean anyMatch(final Identifier id, final String list) {
 		return Arrays.stream(list.split(" "))
-			.filter(entry -> !entry.isBlank())
-			.map(Identifier::of)
+			.map(Identifier::tryParse)
+			.filter(Objects::nonNull)
 			.anyMatch(id::equals);
 	}
 }
